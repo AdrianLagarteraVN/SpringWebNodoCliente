161669efae0108c62cf8621b4adc0c5c618c30ff
Adrian Lagartera <alagartera@viewnext.com>
2020-09-02 13:25:37 +0200

    Creacion Base de Datos H2
    Dispone de una db en memoria que tiene 100 registros que no han sido editados.

---
+++ src/main/java/com/viewnext/main/SpringWebNodoNameApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebNodoNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebNodoNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/main/SpringWebNodoNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebNodoNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
